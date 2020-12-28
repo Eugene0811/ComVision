74f67035da128d21a4606fddaf54b4684fa21c7c
Eugene0811 <74825499+Eugene0811@users.noreply.github.com>
2020-12-28 16:36:59 +0700

    Add files via upload
    เอาไว้เทสรูปว่า detect หน้าในรูปได้ไหม

---
+++ encodingTest.py
@@ -0,0 +1,25 @@
+import cv2
+import numpy as np
+import face_recognition
+import os
+
+path = 'ImageAttendance'
+images = []
+classList = []
+classNames = []
+classId = []
+myList = os.listdir(path)
+print(myList)
+
+for cls in myList:
+    curImg = cv2.imread(f'{path}/{cls}')
+    images.append(curImg)
+    classList.append(os.path.splitext(cls)[0])
+
+def findEncodings(images):
+    encodeList = []
+    for img in images:
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        encode = face_recognition.face_encodings(img)[0]
+        encodeList.append(encode)
+    return encodeList
